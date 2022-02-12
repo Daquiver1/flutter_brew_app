f4158f40f98b27064a00b6d4518d346406770b43
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-02-12 07:36:54 +0000

    Added hints to textform

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:brew_app/services/auth.dart';
+import 'package:brew_app/shared/constants.dart';
 
 class Register extends StatefulWidget {
 
@@ -46,6 +47,7 @@ class _RegisterState extends State<Register> {
 					children: <Widget>[
 					SizedBox(height: 20.0),
 					TextFormField(
+						decoration: textInputDecoration.copyWith(hintText: "Email"),
 						validator: (val) => val!.isEmpty ? "Enter an email" : null,
 						onChanged: (val) {
 							setState(() => email = val);
@@ -53,6 +55,7 @@ class _RegisterState extends State<Register> {
 					),
 					SizedBox(height: 20.0),
 					TextFormField(
+						decoration: textInputDecoration.copyWith(hintText: "Password"),
 						obscureText: true,
 						validator: (val) => val!.length < 6 ? "Enter a password 6+ chars long" : null,
 						onChanged: (val){
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,7 @@
 import 'package:brew_app/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:brew_app/shared/constants.dart';
+
 
 class SignIn extends StatefulWidget {
 
@@ -46,6 +48,7 @@ class _SignInState extends State<SignIn> {
 					children: <Widget>[
 					SizedBox(height: 20.0),
 					TextFormField(
+						decoration: textInputDecoration.copyWith(hintText: "Email"),
 						validator: (val) => val!.isEmpty ? "Enter an email" : null,
 						onChanged: (val) {
 							setState(() => email = val);
@@ -53,6 +56,7 @@ class _SignInState extends State<SignIn> {
 					),
 					SizedBox(height: 20.0),
 					TextFormField(
+						decoration: textInputDecoration.copyWith(hintText: "Password"),
 						validator: (val) => val!.length < 6 ? "Enter a password 6+ chars long" : null,
 						obscureText: true,
 						onChanged: (val){
